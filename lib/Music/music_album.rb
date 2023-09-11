070aa28828528d29c7a657207c895b383d1b09d8
koppai <psthu.koppai@gmail.com>
2023-09-11 22:04:12 +0630

    Refactor code for MusicAlbum class

---
+++ lib/Music/music_album.rb
@@ -0,0 +1,13 @@
+require_relative '../../item'
+
+class MusicAlbum < Item
+  attr_accessor :on_spotify
+
+  def initialize(on_spotify: false)
+    @on_spotify = on_spotify
+  end
+
+  # def can_be_archived?()
+    
+  # end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,2 @@
+require_relative '../lib/Music/music_album'
+require_relative '../lib/Music/genre'
