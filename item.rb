0c5c253a55ca6ce45dfbc34eb8d872e8f1615fe2
Batool Fatima <batoolfatima2135@gmail.com>
2023-09-11 21:07:40 +0500

    Modify item class

--- item.rb
+++ item.rb
@@ -13,15 +13,15 @@ class Item
     @archived = false
   end
 
-  private :can_be_archived?
-
   def move_to_archive
     @archived = true if can_be_archived?
   end
-
+  
   def can_be_archived?
     current_year = Time.now.year
     published_year = @publish_date.year
     current_year - published_year > 10
   end
+
+  private :can_be_archived?
 end
