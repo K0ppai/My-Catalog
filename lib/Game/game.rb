175656019aff72f62ee5c1dfb10be60cf9ae8593
Batool Fatima <batoolfatima2135@gmail.com>
2023-09-11 21:08:02 +0500

    Add game.rb file

---
+++ lib/Game/game.rb
@@ -0,0 +1,25 @@
+require_relative '../../item'
+require 'date'
+
+class Game < Item
+ attr_reader :author, :last_played_at
+  def initialize  multiplayer, last_played_at, publish_date
+    @publish_date = Date.parse(publish_date)
+    @multiplayer = multiplayer
+    @last_played_at = Date.parse(last_played_at)
+  end
+
+  def author=(author)
+    @author = author
+    author.items.push(self)
+  end
+
+  def can_be_archived?
+    parent = super
+    last_played_at_year = @last_played_at.year
+    current_year = Time.now.year
+    parent && current_year - last_played_at_year > 2 ? true : false
+  end
+
+end
+
