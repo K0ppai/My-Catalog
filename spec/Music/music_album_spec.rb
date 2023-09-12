9ee12b58000f9ca7a3b3cede20228a20daf81dd6
koppai <psthu.koppai@gmail.com>
2023-09-12 12:44:23 +0630

    Fix linter error

--- spec/Music/music_album_spec.rb
+++ spec/Music/music_album_spec.rb
@@ -1,18 +1,18 @@
-require_relative "../spec_helper"
+require_relative '../spec_helper'
 
 describe MusicAlbum do
   before :all do
-    @music_album = MusicAlbum.new("2000-12-23", true)
+    @music_album = MusicAlbum.new('2000-12-23', true)
     @can_be_archived = @music_album.can_be_archived?
   end
 
-  context "When creating MusicAlbum class" do
-    it "should have on_spotify instance variable" do
+  context 'When creating MusicAlbum class' do
+    it 'should have on_spotify instance variable' do
       expect(@music_album.on_spotify).to be true
     end
   end
 
-  context "When implementing can_be_archived?() method" do
+  context 'When implementing can_be_archived?() method' do
     it "should return true if parent's method returns true AND if on_spotify equals true" do
       expect(@can_be_archived).to be true
     end
