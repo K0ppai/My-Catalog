da7159c31d565f6bf6adff32d9de1ac1da016701
koppai <psthu.koppai@gmail.com>
2023-09-12 12:28:31 +0630

    Refacotor to push input to collection

--- lib/Music/genre.rb
+++ lib/Music/genre.rb
@@ -1,11 +1,17 @@
 require_relative '../item'
 
 class Genre
+  attr_accessor :items
   attr_reader :name
 
   def initialize(name)
     @id = Random.rand(1..1000)
     @items = []
     @name = name
+  end
+
+  def add_item(item)
+    @items << item if item.instance_of?(Item)
+    item.genre = 
   end
 end
