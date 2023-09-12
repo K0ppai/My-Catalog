7f45faaa8297216d3317473c9a801a9ce3bbb9d3
koppai <psthu.koppai@gmail.com>
2023-09-12 12:27:55 +0630

    Add test for add input item to collection of items

--- spec/Music/genre_spec.rb
+++ spec/Music/genre_spec.rb
@@ -3,6 +3,7 @@ require_relative '../spec_helper'
 describe Genre do
   before :all do
     @genre = Genre.new('abc')
+    @item = Item.new('2000-12-12')
   end
   
   context 'When creating Genre class' do
@@ -13,9 +14,16 @@ describe Genre do
   
   context "When implementing add_item method" do
     it "should take an instance of the Item class as an input" do
-      item = Item.new('2000-12-12')
-      @genre.add_item(item)
-      expect(@genre.add_item(item)).not_to raise_error 
+      expect {@genre.add_item(@item)}.not_to raise_error 
+    end
+
+    it "should add the input item to the collection of items" do
+      @genre.add_item(@item)
+      expect(@genre.items[0]).to be @item 
+    end
+
+    it "should add self as a property of the item object" do
+      
     end
   end
 end
