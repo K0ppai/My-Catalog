d2e54390731b3324b3531421c46cad469e23d769
koppai <psthu.koppai@gmail.com>
2023-09-12 19:32:43 +0630

    Fix linter

--- lib/item.rb
+++ lib/item.rb
@@ -1,7 +1,8 @@
 require 'date'
 
 class Item
-  attr_accessor :archived, :genre, :publish_date
+  attr_accessor :archived, :publish_date
+  attr_reader :genre
 
   def initialize(publish_date)
     @id = Random.rand(1..1000)
