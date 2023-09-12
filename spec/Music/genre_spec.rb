f118b204a87e805e4e4678fa755606f1b0764a2c
koppai <psthu.koppai@gmail.com>
2023-09-12 12:16:57 +0630

    Add test for input in add_item method

--- spec/Music/genre_spec.rb
+++ spec/Music/genre_spec.rb
@@ -4,10 +4,18 @@ describe Genre do
   before :all do
     @genre = Genre.new('abc')
   end
-
+  
   context 'When creating Genre class' do
     it 'should have name instance variable' do
       expect(@genre.name).to eq 'abc'
+    end
+  end
+  
+  context "When implementing add_item method" do
+    it "should take an instance of the Item class as an input" do
+      item = Item.new('2000-12-12')
+      @genre.add_item(item)
+      expect(@genre.add_item(item)).not_to raise_error 
     end
   end
 end
