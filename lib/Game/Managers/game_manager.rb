74d529970a4ddd140e712bc13e9991c25cf2b836
Batool Fatima <batoolfatima2135@gmail.com>
2023-09-12 17:08:42 +0500

    Add game manager class

---
+++ lib/Game/Managers/game_manager.rb
@@ -0,0 +1,26 @@
+require_relative '../game'
+require_relative './author_manager'
+
+class GameManager
+  def initialize
+    @author_manager = AuthorManager.new
+    @all_games = []
+  end
+
+  def list_all_games
+    @all_games.each do |game|
+      puts "Multiplayer: #{game.multiplayer}, published at: #{game.publish_date}, Last played at: #{game.last_played_at} "  
+    end
+  end
+
+   def list_all_authors
+    @author_manager.list_all_authors
+  end
+
+  def add_game(multiplayer, last_played_at, publish_date, first_name, last_name)
+    game = Game.new(multiplayer, last_played_at, publish_date)
+    @author_manager.add_author(game, first_name, last_name)
+    @all_games << game
+  end
+
+end
