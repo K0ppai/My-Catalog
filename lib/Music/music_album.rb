3de66973f762172313b8789af17907945d74bc10
koppai <psthu.koppai@gmail.com>
2023-09-12 12:43:54 +0630

    Refactor code to pass can_be_archived? test

--- lib/Music/music_album.rb
+++ lib/Music/music_album.rb
@@ -3,8 +3,13 @@ require_relative '../item'
 class MusicAlbum < Item
   attr_reader :on_spotify
 
-  def initialize(publish_date, on_spotify: false)
+  def initialize(publish_date, on_spotify)
     super(publish_date)
     @on_spotify = on_spotify
+  end
+
+  def can_be_archived?
+    parent_result = super
+    parent_result && @on_spotify
   end
 end
