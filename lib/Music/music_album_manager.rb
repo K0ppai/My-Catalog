7015093efd2b0c479aedd405e2a933f9f952d589
koppai <psthu.koppai@gmail.com>
2023-09-12 18:59:15 +0630

    Add method to create albums

---
+++ lib/Music/music_album_manager.rb
@@ -0,0 +1,29 @@
+require_relative "music_album"
+require_relative "genre"
+
+class MusicAlbumManager
+  def initialize
+    @albums = []
+  end
+
+  def create_music_album
+    puts "When is it published? Enter data in [yy/mm/dd] format."
+    publish_date = gets.chomp
+    puts "Is it on Spotify? [Y/N]: "
+    on_spotify = gets.chomp.upcase
+    case on_spotify
+    when "Y"
+      music_album = MusicAlbum.new(publish_date, true)
+    when "N"
+      music_album = MusicAlbum.new(publish_date, false)
+    else
+      puts "Please enter a valid input!"
+    end
+    puts "What is the Genre? "
+    genre_name = gets.chomp
+    genre = Genre.new(genre_name)
+    music_album.genre = genre
+    @albums << music_album
+    puts "Music Album Created Successfully!"
+  end
+end
