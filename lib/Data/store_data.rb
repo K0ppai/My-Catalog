bb1e402d71037906932146fded19876b066215bc
koppai <psthu.koppai@gmail.com>
2023-09-12 20:47:31 +0630

    Add store_music_albums method in StoreData class

--- lib/Data/store_data.rb
+++ lib/Data/store_data.rb
@@ -1,1 +1,15 @@
+require 'json'
 
+class StoreData
+  def store_music_albums(albums)
+    music_data = albums.map do |album|
+      {
+        Genre: album.genre.name,
+        Is_On_Spotify: album.on_spotify,
+        Published_Date: album.publish_date,
+      }
+    end
+    json_data = JSON.pretty_generate(music_data)
+    File.write('./lib/Data/JSON/music_albums.json', json_data)
+  end
+end
